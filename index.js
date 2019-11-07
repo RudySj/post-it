81147fba9bdfa8c8f71147a37e01095fc00a8c7f
Rserna97 <rsrn719@gmail.com>
2019-11-06 22:25:17 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// https://colorhunt.co/palette/162836
+/*
+font-family: 'Ultra', serif; --
+font-family: 'Merienda', cursive;
+font-family: 'Chewy', cursive;
+font-family: 'Allerta', sans-serif;
+font-family: 'Ovo', serif; --
+*/
